a1dd0b26ccc4294634045c77fe03c5367603624a
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-10-30 18:57:40 +0100

    feat: docker-compose created, dockerfile modified #1

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "YOU ARE AN IDIOT!!!"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {"message": f"Helloooooooooooo {name.title()}"}
